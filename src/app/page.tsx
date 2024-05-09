3ebb82d0be74e8d68ae4702169e3083829bc9ee8
Marton Bocsi <mvbocsi@gmail.com>
2024-05-08 21:47:55 -0500

    UI is more pleasant now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,64 @@
 "use client";
 
-import { useChat } from "ai/react";
+import { type CoreMessage } from "ai";
+import { useState } from "react";
+import { continueConversation } from "./actions";
+import { readStreamableValue } from "ai/rsc";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Input } from "@/components/ui/input";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [messages, setMessages] = useState<CoreMessage[]>([]);
+  const [input, setInput] = useState("");
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
-        </div>
-      ))}
+    <main className="min-h-screen w-full flex flex-col items-center space-y-4">
+      <div className="w-full py-1 px-4 flex flex-row justify-end">
+        <ModeToggle />
+      </div>
+      <div className="flex flex-col w-full max-w-xl p-4 mx-auto bg-secondary rounded-xl space-y-4">
+        {messages.map((m, i) => (
+          <div
+            className={`flex flex-row justif ${
+              m.role === "user" ? "justify-end" : "justify-start"
+            }`}
+          >
+            <div key={i} className="whitespace-pre-wrap rounded-xl bg-card p-4">
+              {m.content as string}
+            </div>
+          </div>
+        ))}
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
+        <form
+          action={async () => {
+            const newMessages: CoreMessage[] = [
+              ...messages,
+              { content: input, role: "user" },
+            ];
+
+            setMessages(newMessages);
+            setInput("");
+
+            const result = await continueConversation(newMessages);
+
+            for await (const content of readStreamableValue(result)) {
+              setMessages([
+                ...newMessages,
+                {
+                  role: "assistant",
+                  content: content as string,
+                },
+              ]);
+            }
+          }}
+        >
+          <Input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </form>
+      </div>
+    </main>
   );
 }
